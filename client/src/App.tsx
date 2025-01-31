f80d8e59779b99739f1ba6b92cb402360e21d39d
Your Name <marielmetalios@gmail.com>
2025-01-31 18:50:00 -0500

    updated client pages and components structure + updated App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,19 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './components/navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
+// new code to replace below return:
+// return (
+//   <div>
+//     <Navbar />
+//     <main
+//   </div>
+// )
+
+// original code:
   return (
     <>
       <div>
---
+++ client/src/components/navbar.tsx
@@ -0,0 +1,20 @@
+// need to fix imports
+
+const Navbar = () => {
+    return (
+        <section>
+            <header>
+                <div>
+                    <h1>
+                        <p>
+                            <Link>
+                            </Link>
+                        </p>
+                    </h1>
+                </div>
+            </header>
+        </section>
+    )
+};
+
+export default Navbar;
