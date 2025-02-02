7e06162db6426fbde230aadfe11bf39901d8e886
Your Name <marielmetalios@gmail.com>
2025-02-02 15:58:12 -0500

    updated navbar.tsx and installed Bootstrap into Client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,43 +1,45 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import './App.css'
 import Navbar from './components/navbar'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
 // new code to replace below return:
-// return (
-//   <div>
-//     <Navbar />
-//     <main
-//   </div>
-// )
-
-// original code:
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Liftlab</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+return (
+  <div>
+    <Navbar />
+    <h1>Testing</h1>
+  </div>
+)
 }
 
 export default App
+
+// original code:
+  // return (
+//     <>
+//       <div>
+//         <a href="https://vite.dev" target="_blank">
+//           <img src={viteLogo} className="logo" alt="Vite logo" />
+//         </a>
+//         <a href="https://react.dev" target="_blank">
+//           <img src={reactLogo} className="logo react" alt="React logo" />
+//         </a>
+//       </div>
+//       <h1>Liftlab</h1>
+//       <div className="card">
+//         <button onClick={() => setCount((count) => count + 1)}>
+//           count is {count}
+//         </button>
+//         <p>
+//           Edit <code>src/App.tsx</code> and save to test HMR
+//         </p>
+//       </div>
+//       <p className="read-the-docs">
+//         Click on the Vite and React logos to learn more
+//       </p>
+//     </>
+//   )
+// }
--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -1,16 +1,18 @@
-// need to fix imports
+// need to fix 
+// import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 const Navbar = () => {
     return (
-        <section>
-            <header>
-                <div>
-                    <h1>
-                        <p>
-                            <Link>
-                            </Link>
+        <section className = "navbar navbar-expand-lg text-light bg-dark fixed-top" role="navigation" >
+            <header className = "container-fluid">
+                <div >
+                    <h1 className = "navbar-brand text-light">LiftLab</h1>
+                        <p className = "navbar-toggler"> Testing
                         </p>
-                    </h1>
+                    <span className= "d-flex navbar-nav me-auto"> Login </span>
+                    <span className="navbar-nav me-auto"> Home </span>
                 </div>
             </header>
         </section>
