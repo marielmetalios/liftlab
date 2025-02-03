42caa8c46635a6ffb87b331e18efd7c80bcadd46
cbroomew <chad.webster@yahoo.com>
2025-02-03 15:18:00 -0500

    form branch

--- client/src/components/form2.tsx
+++ client/src/components/form2.tsx
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, {useState, FormEvent} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+
 const Form2: React.FC = () => {
+  
+  const [muscleGroup, setMuscleGroup] = useState('Arms');
+  const [equipment, setEquipment] = useState('');
+  const [repsSets, setRepsSets] = useState('');
+  
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    console.log('User submitted the form');
+    console.log('Muscle Group:', muscleGroup);
+    console.log('Equipment:', equipment);
+    console.log('Reps X Sets:', repsSets);
+  };
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div className="form-group">
         <label htmlFor="exampleFormControlSelect1">Select a Muscle Group:</label>
-        <select className="form-control" id="exampleFormControlSelect1">
+        <select className="form-control" id="exampleFormControlSelect1" value={muscleGroup} onChange={(e) => setMuscleGroup(e.target.value)}>
           <option>Arms</option>
           <option>Legs</option>
           <option>Chest</option>
@@ -15,12 +28,14 @@ const Form2: React.FC = () => {
         </select>
       </div>
       <div className="form-group">
-        <label htmlFor="equipmentTextarea">Equipment?</label>
+        <label htmlFor="equipmentTextarea">Workout:</label>
         <textarea
           className="form-control"
           id="equipmentTextarea"
           rows={1}
           required
+          value={equipment}
+          onChange={(e) => setEquipment(e.target.value)}
         ></textarea>
         <div className="invalid-tooltip">
           Please provide a value.
@@ -33,6 +48,8 @@ const Form2: React.FC = () => {
           id="repsSetsTextarea"
           rows={1}
           required
+          value={repsSets}
+          onChange={(e) => setRepsSets(e.target.value)}
         ></textarea>
         <div className="invalid-tooltip">
           Please provide a value.
--- client/src/pages/FormPage.tsx
+++ client/src/pages/FormPage.tsx
@@ -1,4 +1,5 @@
 import MuscleGroupForm from "../components/form1";
+import Form2 from "../components/form2";
 
 
 const FormPage = () => {
@@ -6,6 +7,8 @@ const FormPage = () => {
       <div>
         <h1>Your Workouts</h1>
         <MuscleGroupForm />
+        <h1>Add Workout</h1>
+        <Form2 />
       </div>
     );
   }
