7bd67bbc2647a1690507262ed70acebd355bef1b
cbroomew <chad.webster@yahoo.com>
2025-02-04 11:19:29 -0500

    updated form styling

--- client/src/pages/FormPage.tsx
+++ client/src/pages/FormPage.tsx
@@ -3,14 +3,18 @@ import UserWorkout from "../components/form2";
 
 const formPage = () => {
     return (
-      <div>
-        <h1>Get a Workout</h1>
-        <MuscleGroupForm />
-        <div>
-        <h1>Add a Workout</h1>
-        <UserWorkout />
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6">
+            <h1>Get a Workout</h1>
+            <MuscleGroupForm />
+          </div>
+        <div className="col-md-6">
+            <h1>Log a Workout</h1>
+            <UserWorkout />
         </div>
       </div>
+    </div>
     );
   }
 
