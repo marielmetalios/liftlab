be91f5affa38f4184aa161a794bd2efdab51d7bd
cbroomew <chad.webster@yahoo.com>
2025-02-04 18:43:38 -0500

    added npm package

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,9 +1,11 @@
 import LoginContainer from "../components/login";
+import QuoteContainer from "../components/quoteContainer";
 
 const Home = () => {
     return (
       <div>
         <LoginContainer />
+        <QuoteContainer />
       </div>
     );
   }
